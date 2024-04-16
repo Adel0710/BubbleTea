a508cd5b443e2847228a185d53e4359a10871456
Planchette Alexis <planch_a@piscine-mars-a>
2024-04-16 14:32:24 +0000

    first commit on branch

---
+++ base/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+products = [
+    {'id': 1, 'name': 'BubbleTea 1'},
+    {'id': 2, 'name': 'BubbleTea 2'},
+    {'id': 3, 'name': 'BubbleTea 3'},
+]
+
+def home(request):
+    context = {'products': products}
+    return render(request, 'home.html', context)
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
