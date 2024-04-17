14cc5d5b4e6c0c234d5d484a00b41c350c7f3894
Planchette Alexis <planch_a@piscine-mars-a>
2024-04-17 08:14:06 +0000

    feat dashboard/profile

--- base/views.py
+++ base/views.py
@@ -8,12 +8,22 @@ products = [
     {'id': 3, 'name': 'BubbleTea 3'},
 ]
 
+orders = [
+    {'id': 1, 'name': 'Order 1'},
+    {'id': 2, 'name': 'Order 2'},
+    {'id': 3, 'name': 'Order 3'},
+]
+
 def home(request):
     context = {'products': products}
     return render(request, 'base/home.html', context)
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'base/login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    return render(request, 'base/register.html')
+
+def dashboard(request):
+    context = {'orders': orders}
+    return render(request,'base/dashboard.html', context)
